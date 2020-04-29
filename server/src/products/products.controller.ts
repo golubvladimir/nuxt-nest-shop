cf57d82a1b705c8a9f1c16f9241504011b34064d
Vladimir <golubvlauri@gmail.com>
2020-04-29 16:53:54 +0200

    add products controller

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Param, Get} from '@nestjs/common';
+import {ProductsService} from "./products.service";
+import {Product} from "./product.entity";
 
-@Controller('products')
-export class ProductsController {}
+@Controller('api/products')
+export class ProductsController {
+	constructor(
+		private productsService: ProductsService
+	) {}
+
+	@Get('/')
+	getAllProducts(): Promise<Product[]> {
+		return this.productsService.getAllProducts()
+	}
+
+	@Get('/:id')
+	getProductById(@Param('id') id): Promise<Product> {
+		return this.productsService.getProductById(id);
+	}
+}
