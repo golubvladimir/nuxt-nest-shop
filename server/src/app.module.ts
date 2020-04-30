50af7b8dbaa96c5995c800d7e428baa9cdd5747b
Vladimir <golubvlauri@gmail.com>
2020-04-30 14:05:40 +0200

    add static folder to settings

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesModule } from './categories/categories.module';
+import { ServeStaticModule } from "@nestjs/serve-static";
 import * as ORMConfig from './orm.config';
+import { join } from 'path';
 
 @Module({
   imports: [
+  	ServeStaticModule.forRoot({
+			rootPath: join(__dirname, '..', 'static')
+		}),
 		TypeOrmModule.forRoot(ORMConfig),
   	ProductsModule,
   	CategoriesModule
