66e2e27e8e43636a423cd564415fde0a57babb91
Vladimir <golubvlauri@gmail.com>
2020-05-05 00:03:21 +0200

    add Footer component

---
+++ client/components/common/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Container>
+      <Row>
+        <Column size="12">
+          <p>© NuxtJS NestJS shop</p>
+        </Column>
+      </Row>
+    </Container>
+  </div>
+</template>
+
+<script>
+  import Container from '@/components/grid/Container';
+  import Row from '@/components/grid/Row';
+  import Column from '@/components/grid/Column';
+
+  export default {
+    name: "Footer",
+    components: {
+      Container,
+      Row,
+      Column
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
