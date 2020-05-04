ddb1b2a6763987a897946f89444ae05bcc02bc10
Vladimir <golubvlauri@gmail.com>
2020-05-05 00:03:49 +0200

    add Menu component

---
+++ client/components/common/Menu.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="menu">
+    <Link
+      class="menu__item"
+      v-for="item in items"
+      :to="item.to"
+    >{{ item.title }}</Link>
+  </ul>
+</template>
+
+<script>
+  import Link from '@/components/elements/Link';
+
+  export default {
+    name: "MenuMain",
+    data: () => ({
+      items: [
+        {
+          title: 'Home',
+          to: '/'
+        },
+        {
+          title: 'Catalog',
+          to: '/catalog'
+        }
+      ]
+    }),
+    components: {
+      Link
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .menu {
+    margin: 0;
+    padding: 20px 0;
+
+    &__item {
+      margin-right: 15px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+</style>
