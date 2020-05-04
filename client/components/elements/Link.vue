0b0c8c4692143eaed1c93a4c627e75a08965a761
Vladimir <golubvlauri@gmail.com>
2020-05-05 00:04:01 +0200

    add Link component

---
+++ client/components/elements/Link.vue
@@ -0,0 +1,27 @@
+<template>
+  <nuxt-link
+    class="link"
+    :to="to">
+    <slot />
+  </nuxt-link>
+</template>
+
+<script>
+  export default {
+    name: "Link",
+    props: {
+      to: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .link {
+    text-decoration: none;
+    color: #000;
+
+    &:hover {
+      color: #007bff;
+    }
+  }
+</style>
