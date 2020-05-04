cf3c41527f6886ce20b405f8c22f5462835d7bdd
Vladimir <golubvlauri@gmail.com>
2020-05-04 16:31:10 +0200

    add ProductList component

---
+++ client/components/pages/main/ProductList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="product-list">
+    <ProductCard
+      class="product-list__item"
+      v-for="product in products"
+      :key="product.id"
+      :name="product.name"
+      :src="baseURL + product.photos[0].src"
+      :description="product.description"
+      :price="product.price"
+    />
+  </div>
+</template>
+
+<script>
+  import ProductCard from '@/components/pages/main/ProductCard'
+
+  export default {
+    name: "ProductList",
+    components: {
+      ProductCard
+    },
+    props: {
+      products: Array
+    },
+    computed: {
+      baseURL: function() {
+        return this.$axios.defaults.baseURL;
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .product-list {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    &__item {
+      width: 45%;
+    }
+  }
+</style>
