8315c7f9fd999ca3c07b1a735bfdf2b25b22cefa
Vladimir <golubvlauri@gmail.com>
2020-05-04 16:38:58 +0200

    add content to index page

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,13 +1,40 @@
 <template>
-
+  <div class="main-page">
+    <Container>
+      <Row>
+        <Column size="6">
+          <ProductsList
+            :products="products"
+          />
+        </Column>
+      </Row>
+    </Container>
+  </div>
 </template>
 
 <script>
-export default {
-  components: {
+  import { mapGetters } from 'vuex';
 
+  import Container from '@/components/grid/Container';
+  import Row from '@/components/grid/Row';
+  import Column from '@/components/grid/Column';
+  import ProductsList from '@/components/pages/main/ProductList';
+
+  export default {
+    name: 'main-page',
+    components: {
+      Container,
+      Row,
+      Column,
+      ProductsList
+    },
+    middleware: ['pages/main'],
+    computed: {
+      ...mapGetters({
+        products: 'products/getProducts'
+      })
+    }
   }
-}
 </script>
 
 <style>
