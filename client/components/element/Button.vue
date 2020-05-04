46432c5ccf3e3b55be0f7fabb6af38e949292b92
Vladimir <golubvlauri@gmail.com>
2020-05-04 16:30:52 +0200

    add Button component

---
+++ client/components/element/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    :class="{
+      'btn': true,
+      [`btn-${ level }`]: level
+    }"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "Button",
+    props: {
+      level: String,
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .btn {
+    height: 30px;
+
+    &-primary {
+      background: #007bff;
+    }
+  }
+</style>
