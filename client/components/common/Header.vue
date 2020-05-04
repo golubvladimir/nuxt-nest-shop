40785f22a51c3350a399e724a0e49aa955b926ce
Vladimir <golubvlauri@gmail.com>
2020-05-05 00:03:36 +0200

    add Header component

---
+++ client/components/common/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header-page">
+    <Container>
+      <Row>
+        <Column size="12">
+          <Menu
+            class="header-page__menu"
+          />
+        </Column>
+      </Row>
+    </Container>
+  </div>
+</template>
+
+<script>
+  import Container from '@/components/grid/Container';
+  import Row from '@/components/grid/Row';
+  import Column from '@/components/grid/Column';
+  import Menu from '@/components/common/Menu';
+
+  export default {
+    name: "HeaderPage",
+    components: {
+      Container,
+      Row,
+      Column,
+      Menu
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
