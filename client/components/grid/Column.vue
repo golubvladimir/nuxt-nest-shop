7092f7858450df3484cfc0373e6aa82d5a9c3d2e
Vladimir <golubvlauri@gmail.com>
2020-05-04 10:33:35 +0200

    add to Column component size and offset

--- client/components/grid/Column.vue
+++ client/components/grid/Column.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="column">
+  <div :class="{
+    'column': true,
+    [`column-${ size }`]: size,
+    [`column-offset-${ offset }`]: offset
+  }">
     <slot />
   </div>
 </template>
 
 <script>
   export default {
-    name: "Col"
+    name: "Col",
+    props: {
+      size: String,
+      offset: String
+    }
   }
 </script>
 
@@ -16,5 +24,103 @@
     padding-right: 15px;
     padding-left: 15px;
     position: relative;
+
+    &-offset {
+      &-12 {
+        margin-left: (100% / 12) * 12;
+      }
+
+      &-11 {
+        margin-left: (100% / 12) * 11;
+      }
+
+      &-10 {
+        margin-left: (100% / 12) * 10;
+      }
+
+      &-9 {
+        margin-left: (100% / 12) * 9;
+      }
+
+      &-8 {
+        margin-left: (100% / 12) * 8;
+      }
+
+      &-7 {
+        margin-left: (100% / 12) * 7;
+      }
+
+      &-6 {
+        margin-left: (100% / 12) * 6;
+      }
+
+      &-5 {
+        margin-left: (100% / 12) * 5;
+      }
+
+      &-4 {
+        margin-left: (100% / 12) * 4;
+      }
+
+      &-3 {
+        margin-left: (100% / 12) * 3;
+      }
+
+      &-2 {
+        margin-left: (100% / 12) * 2;
+      }
+
+      &-1 {
+        margin-left: (100% / 12) * 1;
+      }
+    }
+
+    &-12 {
+      width: (100% / 12) * 12;
+    }
+
+    &-11 {
+      width: (100% / 12) * 11;
+    }
+
+    &-10 {
+      width: (100% / 12) * 10;
+    }
+
+    &-9 {
+      width: (100% / 12) * 9;
+    }
+
+    &-8 {
+      width: (100% / 12) * 8;
+    }
+
+    &-7 {
+      width: (100% / 12) * 7;
+    }
+
+    &-6 {
+      width: (100% / 12) * 6;
+    }
+
+    &-5 {
+      width: (100% / 12) * 5;
+    }
+
+    &-4 {
+      width: (100% / 12) * 4;
+    }
+
+    &-3 {
+      width: (100% / 12) * 3;
+    }
+
+    &-2 {
+      width: (100% / 12) * 2;
+    }
+
+    &-1 {
+      width: (100% / 12) * 1;
+    }
   }
 </style>
