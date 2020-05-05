607224d5f1806fba7eaaae3262547127d2871069
Vladimir <golubvlauri@gmail.com>
2020-05-05 11:25:57 +0200

    add Cart component

---
+++ client/components/common/Cart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="cart">
+    <Link class="cart__link" to="/card">
+      <img
+        class="cart__icon"
+        src="~static/icons/cart.svg" alt="Cart"
+      />
+      <p class="cart__qnt">0</p>
+    </Link>
+  </div>
+</template>
+
+<script>
+  import Link from '@/components/elements/Link';
+
+  export default {
+    name: "Cart",
+    components: {
+      Link
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .cart {
+
+    &__link {
+      display: flex;
+    }
+
+    &__icon {
+      width: 20px;
+      height: 20px;
+      margin-right: 10px;
+    }
+  }
+</style>
