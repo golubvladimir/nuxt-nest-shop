1b13807bbf1c66d511dc32f30479f340f3fdee18
Vladimir <golubvlauri@gmail.com>
2020-05-06 08:33:48 +0200

    add cart page

---
+++ client/pages/cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cart-page">
+    <h1 class="cart-page__title">Cart</h1>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "cart"
+    }
+</script>
+
+<style scoped>
+
+</style>
