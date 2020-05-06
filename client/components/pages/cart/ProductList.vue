9343dd4c1b218dd910a593db337f3c84f158ccc5
Vladimir <golubvlauri@gmail.com>
2020-05-06 08:34:06 +0200

    add ProductList component

---
+++ client/components/pages/cart/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="product-list">
+    <ProductItem
+      class="product-list__item"
+      v-for="product in products"
+      :name="product.name"
+      :qnt="product.qnt"
+      :price="product.price"
+    />
+  </div>
+</template>
+
+<script>
+  import ProductItem from '@/components/pages/cart/ProductItem';
+
+  export default {
+    name: "ProductList",
+    props: {
+      products: Array
+    },
+    components: {
+      ProductItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
