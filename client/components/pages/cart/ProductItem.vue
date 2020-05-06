78ccea289aad80d7f0aa9aac28bc87f79f017bb8
Vladimir <golubvlauri@gmail.com>
2020-05-06 08:34:19 +0200

    add ProductItem component

---
+++ client/components/pages/cart/ProductItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="product-item">
+    <img
+      class="product-item__img"
+      :src="src"
+      :alt="name"
+    />
+    <p class="product-item__name">{{ name }}</p>
+    <p class="product-item__qnt">{{ qnt }}</p>
+    <p class="product-item__price">{{ price * qnt }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProductItem",
+    props: {
+      name: String,
+      src: String,
+      qnt: Number,
+      price: Number
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .product-item {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
