7bd59fbedea400ae5d9291aa78092c3360afbcec
Vladimir <golubvlauri@gmail.com>
2020-05-07 11:10:47 +0200

    add pagination to getProducts method

--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Product} from "./product.entity";
 import {Repository} from "typeorm";
+import {getProducts} from "./products.interface";
 
 @Injectable()
 export class ProductsService {
@@ -10,8 +11,22 @@ export class ProductsService {
 		private productsRepository: Repository<Product>
 	) {}
 
-	getAllProducts(): Promise<Product[]> {
-		return this.productsRepository.find({ relations: ["category", "photos"] });
+	async getProducts(page: number, perPage: number): Promise<getProducts> {
+		const products = await this.productsRepository.find({
+			take: perPage,
+			skip: page * perPage,
+			relations: ["category", "photos"] }
+		);
+
+		const total = await this.productsRepository.count();
+
+		const totalPage = Math.ceil(total/perPage);
+
+		return {
+			products,
+			total,
+			totalPage
+		}
 	}
 
 	getProductById(id: string): Promise<Product> {
