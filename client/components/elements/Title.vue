85032e957acacb1c7337bc60cd97eed886340f2f
Vladimir <golubvlauri@gmail.com>
2020-05-07 16:49:26 +0200

    add Title component

---
+++ client/components/elements/Title.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1
+    v-if="size === '1'"
+    class="title"
+  >
+    <slot />
+  </h1>
+  <h2
+    v-else-if="size === '2'"
+    class="title"
+  >
+    <slot />
+  </h2>
+  <h3
+    v-else-if="size === '3'"
+    class="title"
+  >
+    <slot />
+  </h3>
+  <h4
+    v-else-if="size === '4'"
+    class="title"
+  >
+    <slot />
+  </h4>
+  <h5
+    v-else-if="size === '5'"
+    class="title"
+  >
+    <slot />
+  </h5>
+  <h6
+    v-else-if="size === '6'"
+    class="title"
+  >
+    <slot />
+  </h6>
+</template>
+
+<script>
+  export default {
+    name: "Title",
+    props: {
+      size: String
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
