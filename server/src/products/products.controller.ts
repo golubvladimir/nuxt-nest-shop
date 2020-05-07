1d9f75f343ca5b6a0d1361f753ccacb25f6f0df6
Vladimir <golubvlauri@gmail.com>
2020-05-07 15:51:45 +0200

    fix products controller
    add params

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,6 +1,7 @@
-import {Controller, Param, Get} from '@nestjs/common';
+import {Controller, Param, Get, Query} from '@nestjs/common';
 import {ProductsService} from "./products.service";
 import {Product} from "./product.entity";
+import {getProducts} from "./products.interface";
 
 @Controller('products')
 export class ProductsController {
@@ -9,8 +10,8 @@ export class ProductsController {
 	) {}
 
 	@Get('/')
-	getAllProducts(): Promise<Product[]> {
-		return this.productsService.getAllProducts()
+	getProducts(@Query('page') page, @Query('perPage') perPage): Promise<getProducts> {
+		return this.productsService.getProducts(page, perPage);
 	}
 
 	@Get('/:id')
