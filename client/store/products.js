21a44dcefbf124eab155d62588dff42bfa583206
Vladimir <golubvlauri@gmail.com>
2020-05-07 15:55:48 +0200

    fix store module - products
    add new items

--- client/store/products.js
+++ client/store/products.js
@@ -1,19 +1,43 @@
-export const state = () => {
-  products: []
-};
+export const state = () => ({
+  products: [],
+  page: 0,
+  perPage: 4,
+  total: 0,
+  totalPage: 0
+});
 
 export const getters = {
-  getProducts: state => state.products
+  getProducts: state => state.products,
 };
 
 export const mutations = {
-  setProducts: (state, products) => state.products = products
+  setProducts: (state, products) => state.products = products,
+  setTotal: (state, total) => state.total = total,
+  setTotalPage: (state, totalPage) => state.totalPage = totalPage
 };
 
 export const actions = {
-  async getProducts({ commit }) {
-    const AllProducts = await this.$axios.$get('api/products');
+  async getProducts({ commit, state }) {
+    const products = await this.$axios.$get('api/products', {
+      params: {
+        page: state.page,
+        perPage: state.perPage
+      }
+    });
 
-    commit('setProducts', AllProducts);
+    commit('setProducts', products);
   },
+  async getProductsMain({ commit }) {
+    const result = await this.$axios.$get('api/products', {
+      params: {
+        page: 0,
+        perPage: 4
+      }
+    });
+
+    commit('setProducts', result.products);
+    commit('setTotal', result.total);
+    commit('setTotalPage', result.totalPage);
+  }
+
 };
