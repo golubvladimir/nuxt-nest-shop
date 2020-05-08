b08fda15aad3c3aefef99954c7e01256519438f5
Vladimir <golubvlauri@gmail.com>
2020-05-08 14:43:45 +0200

    fix catalog middleware

--- client/middleware/pages/catalog.js
+++ client/middleware/pages/catalog.js
@@ -1,6 +1,9 @@
 export default async function ({ store }) {
   if (!store.state.products.products.length) {
     await store.dispatch('products/getProducts');
+  }
+
+  if (!store.state.products.categories.length) {
     await store.dispatch('products/getCategories');
   }
 }
