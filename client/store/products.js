ba98c1ec4404ce5a1c41ed6ed4980bdb405f0ce9
Vladimir <golubvlauri@gmail.com>
2020-05-08 13:31:27 +0200

    add new action to products store module - get categories

--- client/store/products.js
+++ client/store/products.js
@@ -1,5 +1,6 @@
 export const state = () => ({
   products: [],
+  categories: [],
   page: 0,
   perPage: 8,
   total: 0,
@@ -13,7 +14,8 @@ export const getters = {
 export const mutations = {
   setProducts: (state, products) => state.products = products,
   setTotal: (state, total) => state.total = total,
-  setTotalPage: (state, totalPage) => state.totalPage = totalPage
+  setTotalPage: (state, totalPage) => state.totalPage = totalPage,
+  setCategories: (state, categories) => state.ca
 };
 
 export const actions = {
@@ -28,6 +30,11 @@ export const actions = {
     commit('setProducts', result.products);
     commit('setTotal', result.total);
     commit('setTotalPage');
+  },
+  async getCategories({ commit }) {
+    const categories = await this.$axios.$get('api/categories');
+
+    commit('setCategories', categories);
   },
   async getProductsMain({ commit }) {
     const result = await this.$axios.$get('api/products', {
