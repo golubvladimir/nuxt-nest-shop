2960af4cc0e8a0f564aa5b99ed1d0926427669ca
Vladimir <golubvlauri@gmail.com>
2020-05-08 14:44:08 +0200

    fix products store module

--- client/store/products.js
+++ client/store/products.js
@@ -9,13 +9,14 @@ export const state = () => ({
 
 export const getters = {
   getProducts: state => state.products,
+  getCategories: state => state.categories
 };
 
 export const mutations = {
   setProducts: (state, products) => state.products = products,
   setTotal: (state, total) => state.total = total,
   setTotalPage: (state, totalPage) => state.totalPage = totalPage,
-  setCategories: (state, categories) => state.ca
+  setCategories: (state, categories) => state.categories = categories
 };
 
 export const actions = {
@@ -33,7 +34,7 @@ export const actions = {
   },
   async getCategories({ commit }) {
     const categories = await this.$axios.$get('api/categories');
-
+    
     commit('setCategories', categories);
   },
   async getProductsMain({ commit }) {
